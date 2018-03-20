34ceac5f54dfeef2a18b8bfd2290ecfaad699e74
Huy Vo <huyvoxuan@hotmail.com>
2018-03-21 04:31:51 +0800

    add debug

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function reducer(state = {count: 10}, action) {
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
 	reducer, /* preloadedState, */
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 /* eslint-enable */
 class App extends Component {
